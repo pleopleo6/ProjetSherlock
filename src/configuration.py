570c5c9530a8febf8fbc6a6c56548cd154a0e7c2
Leo Pellandini <leo.pellandini@unisante.ch>
2026-05-04 10:46:17 +0200

    conf done simple

--- src/configuration.py
+++ src/configuration.py
@@ -1,17 +1,8 @@
 """This module provides the Configuration singleton class."""
 
-from __future__ import annotations
-
-import datetime
-
 __email__ = "kevin.huguenin@unil.ch"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2026, The Information Security and Privacy Lab at the University of Lausanne (https://www.unil.ch/isplab/)"
-
-from typing import TYPE_CHECKING, override
-
-if TYPE_CHECKING:
-    from datetime import date, datetime
 
 
 # TODO: Créer une classe Configuration contenant:
@@ -25,28 +16,44 @@ if TYPE_CHECKING:
 # TODO: Utiliser le patron de conception Singleton pour cette classe, pour
 #       manipuler la configuration de manière globale dans tout le programme.
 class Configuration:
-    def __init__() -> None: ...
 
-    def add_element() -> None: ...
+    _instance = None #singleton
+    _dico: dict = {}
 
-    def get_element() -> bool | str | float | date | datetime | None: ...
+    def __init__(self):
+        self._dico = {}
+
+    @classmethod
+    def get_instance(configuration):
+        if configuration._instance == None:
+            configuration._instance = Configuration()
+        return configuration._instance
+
+    def add_element(self, cle, valeur):
+        self._dico[cle] = valeur
+
+    def get_element(self, cle, autreArgument=None): #pas sur de comment faire 
+        if cle in self._dico:
+            return self._dico[cle]
+        return autreArgument
+
+    def __str__(self):
+        return str(self._dico)
 
 
 if __name__ == "__main__":
-    pass
-    # conf: Configuration = Configuration.get_instance()
+    conf = Configuration.get_instance()
 
-    # conf.add_element('verbose', True)
-    # conf.add_element('N', 6)
-    # print(conf)
+    conf.add_element('verbose', True)
+    conf.add_element('N', 6)
+    print(conf)
 
-    # max = conf.get_element('max', 42)
-    # print(max, conf.get_element('max'), conf.get_element('verbose'))
+    max = conf.get_element('max', 42) #pas compris les 2 arguments ? valeur défaut ?
+    print(max, conf.get_element('max'), conf.get_element('verbose'))
 
-    # print(conf == Configuration.get_instance())
+    print(conf == Configuration.get_instance())
 
     ### Résultat attendu ###
-
     # {'verbose': True, 'N': 6}
     # 42 None True
     # True
